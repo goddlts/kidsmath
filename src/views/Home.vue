e1ecc5507552f4bb1cf2d5b61736cfb7e3e19808
xxx <xxx@xxx.com>
2020-02-05 21:13:41 +0800

    exam

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,27 @@
   <van-tabs v-model="activeName">
     <van-tab title="10以内" name="x10">
       <van-field
+        type="digit"
         v-for="obj in x10"
+        clearable
         :key="obj.id"
         label-width="50%"
         v-model.number="obj.user"
         placeholder="请输入结果">
         <div slot="label">{{obj.x}} {{obj.o}} {{obj.y}} =  </div>
         <div slot="right-icon">
-          <van-icon v-if="obj.r === obj.user" name="success" />
-          <van-icon v-else-if="obj.user !== null && obj.r !== obj.user" name="cross" />
+          <div v-if="flag === 'view'">
+            <van-icon v-if="obj.r === obj.user" name="success" />
+            <van-icon v-else-if="obj.user !== null && obj.r !== obj.user" name="cross" />
+          </div>
         </div>
       </van-field>
       <van-button @click="changeX10" type="primary" block>换一换</van-button>
     </van-tab>
     <van-tab title="20以内" name="x20">
        <van-field
+        type="digit"
+        clearable
         v-for="obj in x20"
         :key="obj.id"
         label-width="50%"
@@ -24,8 +30,10 @@
         placeholder="请输入结果">
         <div slot="label">{{obj.x}} {{obj.o}} {{obj.y}} =  </div>
         <div slot="right-icon">
-          <van-icon v-if="obj.r === obj.user" name="success" />
-          <van-icon v-else-if="obj.user !== null && obj.r !== obj.user" name="cross" />
+          <div v-if="flag === 'view'">
+            <van-icon v-if="obj.r === obj.user" name="success" />
+            <van-icon v-else-if="obj.user !== null && obj.r !== obj.user" name="cross" />
+          </div>
         </div>
       </van-field>
       <van-button @click="changeX20" type="primary" block>换一换</van-button>
@@ -71,7 +79,11 @@ export default {
       x20: [],
       histories: [],
       // 历史记录的所有 key
-      keys: []
+      keys: [],
+      // 是否显示对错图标
+      // exam 不显示
+      // view 显示
+      flag: 'exam'
     }
   },
   created () {
@@ -82,9 +94,11 @@ export default {
   methods: {
     changeX10 () {
       this.x10 = getRandomArray(10, 0, 10)
+      this.flag = 'exam'
     },
     changeX20 () {
       this.x20 = getRandomArray(10, 11, 20)
+      this.flag = 'exam'
     },
     handleSave () {
       save({
@@ -97,12 +111,15 @@ export default {
       this.x10 = getRandomArray(10, 0, 10)
       this.x20 = getRandomArray(10, 11, 20)
       this.activeName = 'x10'
+      this.flag = 'exam'
     },
     restore (key) {
       let obj = getKey(key)
       this.x10 = obj.x10
       this.x20 = obj.x20
       this.activeName = 'x10'
+
+      this.flag = 'view'
     },
     handleRemove (key) {
       this.keys = remove(key)
